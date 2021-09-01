9fa1c37d37a1c80079e2da04f60ff3c99409d648
JuniorChang <changjiale@gmail.com>
2021-09-01 06:53:40 +0000

    adding routes

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,13 +4,33 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 // Get Posts
-router.get('/', (req,res) => {
-    res.send('hello');
+router.get('/', async (req,res) => {
+    const posts = await loadPostCollection();
+    res.send(await posts.find({}).toArray());
 });
 
 // Add Post
+router.post('/', async(req,res) => {
+    const posts = await loadPostCollection();
+    await this.post.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+})
 
 
 // Delete post
+router.delete('/:id', async (req,res) => {
+    const posts = await loadPostCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectId(req.params.id)});
+    res.status(200).send();
+})
+
+async function loadPostCollection() {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://Jr:simplemongopassword@class31stjuly.myynu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser : true});
+
+    return client.db('myFirstDataBase').collection('posts');
+}
 
 module.exports = router;
