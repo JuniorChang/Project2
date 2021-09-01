8e6394e3b049c31720697680fb9f3d7974ceb563
JuniorChang <changjiale@gmail.com>
2021-09-01 07:14:00 +0000

    formatting

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,36 +1,38 @@
-const express = require('express');
-const mongodb = require('mongodb');
+const express = require("express");
+const mongodb = require("mongodb");
 
 const router = express.Router();
 
 // Get Posts
-router.get('/', async (req,res) => {
-    const posts = await loadPostCollection();
-    res.send(await posts.find({}).toArray());
+router.get("/", async (req, res) => {
+  const posts = await loadPostCollection();
+  res.send(await posts.find({}).toArray());
 });
 
 // Add Post
-router.post('/', async(req,res) => {
-    const posts = await loadPostCollection();
-    await this.post.insertOne({
-        text: req.body.text,
-        createdAt: new Date()
-    });
-    res.status(201).send();
-})
-
+router.post("/", async (req, res) => {
+  const posts = await loadPostCollection();
+  await this.post.insertOne({
+    text: req.body.text,
+    createdAt: new Date(),
+  });
+  res.status(201).send();
+});
 
 // Delete post
-router.delete('/:id', async (req,res) => {
-    const posts = await loadPostCollection();
-    await posts.deleteOne({_id: new mongodb.ObjectId(req.params.id)});
-    res.status(200).send();
-})
+router.delete("/:id", async (req, res) => {
+  const posts = await loadPostCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectId(req.params.id) });
+  res.status(200).send();
+});
 
 async function loadPostCollection() {
-    const client = await mongodb.MongoClient.connect('mongodb+srv://Jr:simplemongopassword@class31stjuly.myynu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser : true});
+  const client = await mongodb.MongoClient.connect(
+    "mongodb+srv://Jr:simplemongopassword@class31stjuly.myynu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  );
 
-    return client.db('myFirstDataBase').collection('posts');
+  return client.db("myFirstDataBase").collection("posts");
 }
 
 module.exports = router;
