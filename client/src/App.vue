2c4afe2b735384c9460ca6bc2b3b2d4bcaab9ce5
JuniorChang <changjiale@gmail.com>
2021-09-02 07:15:13 +0000

    update app.vue and top banner

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <PostComponent />
+  <div>
+    <img class="img-fluid topbanner" src="./assets/topbanner.jpg">
+
+    <div class="container-fluid">
+      
+
+      <div class="alert alert-success my-3" v-if="status">
+        {{ status }}
+      </div>
+      <ul class="nav nav-tabs my-3">
+        <li class="nav-item">
+          <button
+            class="nav-link active"
+            aria-current="page"
+            v-on:click="goRecipes"
+          >
+            Wild Path
+          </button>
+        </li>
+        <li class="nav-item">
+          <button class="nav-link" v-on:click="goAddRecipe">Wild Exile</button>
+        </li>
+      </ul>
+   
+    <!-- <PostComponent /> -->
+
+    <wildpath v-if="page == 'Wildpage'" v-on:update-recipe="onEditRecipe" />
+    <AddRecipe v-if="page == 'add'" v-on:new-recipe-added="onRecipeAdded" />
+
+    </div>
   </div>
+  
 </template>
 
 <script>
-import PostComponent from './components/PostComponent.vue'
+// import PostComponent from "./components/PostComponent.vue";
+import Wildpath from "./components/WildPath.vue";
+
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    PostComponent
-  }
-}
+    // PostComponent,
+    Wildpath,
+  },
+  data: function () {
+    return {
+      page: "WildPath",
+      status: "",
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.topbanner {
+  min-width: 100% ;
 }
+
+
 </style>
---
+++ client/src/components/WildPath.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1> Testing </h1>
+        <div> 
+            <label class="form-label"> Name </label>
+            <input class="form-control" type="text" v-model="name" />
+        </div>
+        <div>
+            <label class="form-label"> Server </label>
+            <input class="form-control" type="text" v-model="server" />
+        </div>
+        <button class="mt-3 btn btn-primary" v-on:click="processWildPath">
+            Add
+        </button>
+
+    </div>
+</template>
+
+<script>
+// import axios from "axios";
+
+// const API_URL = ""
+
+export default {
+    // name: "WildPath",
+    // data: function() {
+    //     return {
+    //         name: "",
+    //     };
+    // },
+    // methods: {
+    //     processWildPath: async function () {
+    //         await axios.post(API_URL + "/WildPath",{
+    //             Name: this.name,
+    //             Server: this.server,
+
+    //         });
+    //         this.$emit("new-data-added");
+    //     }
+    // }
+}
+</script>
